48b3264f27a85ac065835df9ae7337e0d5f5566b
Ignacio Bona <igbopie@gmail.com>
2015-09-27 20:30:37 -0700

    Initial Commit

---
+++ GWatchMeter WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  InterfaceController.swift
+//  GWatchMeter WatchKit Extension
+//
+//  Created by Ignacio Bona Piedrabuena on 9/27/15.
+//  Copyright © 2015 Ignacio Bona Piedrabuena. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awakeWithContext(context: AnyObject?) {
+        super.awakeWithContext(context)
+        
+        // Configure interface objects here.
+    }
+
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
